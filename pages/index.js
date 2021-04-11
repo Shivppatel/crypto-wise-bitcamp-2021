337b18126280b14ecad4a9b5b4d4d7a9ca512a4c
shivppatel <shiv.p.patel@outlook.com>
2021-04-11 11:42:54 -0400

    cleaned up ignore files

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
               </div>
               <div class='hidden md:block md:ml-10 md:pr-4 md:space-x-8'>
                 <a
-                  href='#'
+                  href='/'
                   className='font-medium text-gray-500 hover:text-gray-900'
                 >
                   Home
@@ -141,7 +141,7 @@ export default function Home() {
               </div>
               <div class='hidden md:block md:ml-10 md:pr-4 md:space-x-8'>
                 <a
-                  href='#'
+                  href='/'
                   className='font-medium text-gray-500 hover:text-gray-900'
                 >
                   Home
